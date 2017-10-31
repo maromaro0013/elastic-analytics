2bda01b1fac9b60b13781d8b868dc0f7bf623a63
maromaro0013 <ota0120@gmail.com>
2017-10-31 20:24:41 +0900

    pumaの設定を変更

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+pidfile "/var/www/elastic-analytics/shared/elastic-analytics.pid"
+bind "unix:///var/www/elastic-analytics/shared/elastic-analytics.sock"
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
