f914d1b72a299ecb293b8eec3cbbff50c9853d91
maromaro0013 <ota0120@gmail.com>
2017-10-31 20:12:00 +0900

    pvを取得するメソッドを作成

--- app/controllers/pv_controller.rb
+++ app/controllers/pv_controller.rb
@@ -1,6 +1,46 @@
 class PvController < ApplicationController
 
   def get
-    render :json => {result: "testdesu"}.to_json
+    client = Elasticsearch::Client.new(
+      log: true, logger: Logger.new("log/Elasticsearch.log"), host: ENV["ELASTICSEARCH_HOST"])
+
+    begin
+      gte = DateTime.parse(params[:gte]) - Rational(9, 24)
+      lte = DateTime.parse(params[:lte]) - Rational(9, 24) - Rational(1, 24*60*60)
+    rescue
+      render :json => { result: nil, err: "illegal date format"}.to_json
+      return
+    end
+
+    body = {
+      size: 0,
+      query: {
+        bool: {
+          must: [
+            {range: {
+              time_iso8601: {
+                gte: gte.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                lte: lte.strftime("%Y-%m-%dT%H:%M:%SZ")
+              }
+            }},
+            {match: { "type.keyword": "landing" }},
+            {terms: { "dashboard_flg": [0,2] }}
+          ]
+        }
+      }
+    }
+
+    index = "axs_#{params[:client_id]}"
+
+    begin
+      ret = client.search(index: index, type: 'access_log', body: body)
+    rescue => e
+      render :json => { result: nil, err: e.to_s }.to_json
+      return
+    end
+
+    render :json => {
+      result: ret,
+    }.to_json
   end
 end
